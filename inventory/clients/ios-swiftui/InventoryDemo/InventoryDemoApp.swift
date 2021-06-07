8eea3654d89266fc481f701e4c3dbbf3f4ae81e2
Lauren Schaefer <lauren.schaefer@mongodb.com>
2021-06-07 14:21:38 -0400

    Do nothing

--- inventory/clients/ios-swiftui/InventoryDemo/InventoryDemoApp.swift
+++ inventory/clients/ios-swiftui/InventoryDemo/InventoryDemoApp.swift
@@ -12,7 +12,7 @@ import RealmSwift
 // called "realm-app-id.txt" and put inside a line with the realm app id you've created
 // that id will look like myappsync-xxxxx
 let (success, filecontent) = readFile(named: "realm-app-id.txt")
-
+ 
 let app = RealmSwift.App(id: success ? filecontent: "Couldn't read secrets file. Is there?" )
 var store = "101"
 
